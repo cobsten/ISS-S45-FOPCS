5a53254ab1e93036945e7d47b79d179cc4b7f813
cobsten <cobsten@gmail.com>
2017-08-15 17:54:19 +0800

    Add project files.

---
+++ ConsoleApplication1/WSQ1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class WSQ1
+    {
+        static void Main()
+        {
+            System.Console.WriteLine("Winston Tan");
+            System.Console.WriteLine("e0227389@u.nus.edu");
+        }
+    }
+}
---
+++ ConsoleApplication1/WSQ2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class WSQ2
+    {
+        static void Main()
+        {
+            System.Console.WriteLine("Good Morning {0}", System.Console.ReadLine());
+        }
+    }
+}
